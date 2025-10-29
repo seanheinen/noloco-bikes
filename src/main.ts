7cc5b5022cac995beefab97fa163392edfa7f2d3
seanheinen <seanheinen88@gmail.com>
2025-10-29 19:36:49 +0100

    Fix swagger copy

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle('Dublink bike stations API')
+    .setVersion('1.0.0')
+    .addTag('Dubit')
+    .addTag('Bike')
+    .addTag('Stations')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
